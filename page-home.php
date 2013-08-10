4a4262bef5f26edc57c8a6b3827deed03fc66059
ACF Sysadmin <sysadmin+git@acfhi.org>
2013-08-10 07:04:11 -0700

    Updates to page-home.php

--- page-home.php
+++ page-home.php
@@ -14,13 +14,39 @@ get_header(); ?>
     <div class="container">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
       <h1><?php the_title();?></h1>
-      <?php the_content();?>
+      <?php //the_content();
+        // Retrieve the custom field(s) 'Tagline' and print those here instead
+        // of the posts's content:
+        $custom_fields = get_post_custom();
+        $tagline = $custom_fields['Tagline'];
+        foreach ( $tagline as $key => $value )
+          echo "<p>" . $value . "</p>";
+      ?>
     </div>
 </div>
 
 <?php endwhile; endif; ?>
+
+<div class="bs-docs-social">
+	<div class="container">
+      <ul class="bs-docs-social-buttons">
+         <li class="follow-btn">
+            <a class="twitter-follow-button" href="http://twitter.com/acfhawaii">Follow @acfhawaii</a>
+         </li>
+         <li><a class="btn btn-success" href="/donate">Donate!</a></li>
+      </ul>
+   </div>
+</div> <!-- bs-docs-social -->
+
 <div class="container">
-  <div class="marketing">
+  <!--div class="marketing"-->
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php the_content();?>
+    <?php endwhile; endif; ?>
+  <!--/div--><!-- /.marketing -->
+</div>
+
+<div class="container"> 
   <div class="row-fluid">
     <div class="span4">
       <?php
@@ -38,6 +64,5 @@ if ( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "home-right" );
 ?>
     </div>
   </div>
-</div><!-- /.marketing -->
 </div>
 <?php get_footer();?>
